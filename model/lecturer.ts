19bbfbd1eae770c70851bdc66bdc63a61c2160a1
johanpck <johanpieck@gmail.com>
2022-02-22 21:39:57 +0100

    Add lecturer functionality

--- model/lecturer.ts
+++ model/lecturer.ts
@@ -1,6 +1,6 @@
 import { Lecturer } from '../types';
 import { query } from '../database';
-import { RowDataPacket } from 'mysql2';
+import { OkPacket, RowDataPacket } from 'mysql2';
 
 const mapRowsToLecturers = (rows: RowDataPacket[]): Array<Lecturer> => {
     const result = [];
@@ -38,15 +38,32 @@ const mapRowsToLecturers = (rows: RowDataPacket[]): Array<Lecturer> => {
 };
 
 const getLecturers = (callback: Function) => {
-    const queryString = `SELECT l.id AS lecturer_id, l.name AS lecturer_name, c.id AS course_id, c.name AS course_name, c.description AS course_description, c.phase AS course_phase
+    const queryStatement = `SELECT l.id AS lecturer_id, l.name AS lecturer_name, c.id AS course_id, c.name AS course_name, c.description AS course_description, c.phase AS course_phase
   FROM lecturer AS l, course AS c, lecturer_course AS lc
   WHERE l.id = lc.lecturer_id
   AND c.id = lc.course_id`;
 
     (async () => {
-        const rows = await query(queryString);
+        const rows = await query(queryStatement);
         callback(null, mapRowsToLecturers(rows));
     })().catch((err) => callback(err));
 };
 
-export { getLecturers };
+const addLecturer = (lecturer: Lecturer, callback: Function) => {
+    const lecturerInsert = 'INSERT INTO lecturer (name) VALUES (?)';
+    const lecturerCourseInsert =
+        'INSERT INTO lecturer_course (lecturer_id, course_id) VALUES (?, ?)';
+
+    (async () => {
+        const result = await query(lecturerInsert, [lecturer.name]);
+        const addedLecturerId = (<OkPacket>result).insertId;
+
+        lecturer.courses.forEach(
+            async (course) => await query(lecturerCourseInsert, [addedLecturerId, course.id])
+        );
+
+        callback(null, addedLecturerId);
+    })().catch((err) => callback(err));
+};
+
+export { getLecturers, addLecturer };
--- routes/lecturer-router.ts
+++ routes/lecturer-router.ts
@@ -1,3 +1,32 @@
+/**
+ * @swagger
+ *   components:
+ *    schemas:
+ *      Lecturer:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: number
+ *            name:
+ *              type: string
+ *              description: Lecturer's name.
+ *            courses:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: number
+ *                  name:
+ *                    type: string
+ *                    description: Course name
+ *                  description:
+ *                    type: string
+ *                    description: Course description
+ *                  phase:
+ *                    type: number
+ *                    description: The phase within the education path
+ */
 import express, { Request, Response, Handler } from 'express';
 import * as lecturerModel from '../model/lecturer';
 import { Lecturer } from '../types';
@@ -15,33 +44,46 @@ const lecturerRouter = express.Router();
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 name:
- *                   type: string
- *                   description: Lecturer's name.
- *                 courses:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       name:
- *                         type: string
- *                         description: Course name
- *                       description:
- *                         type: string
- *                         description: Course description
- *                       phase:
- *                         type: number
- *                         description: The phase within the education path
+ *               $ref: '#/components/schemas/Lecturer'
  */
 lecturerRouter.get('/', (req: Request, res: Response) => {
     lecturerModel.getLecturers((err: Error, lecturers: Array<Lecturer>) => {
         if (err) {
-            return res.status(500).json({ errorMessage: err.message });
+            return res.status(500).json({ status: 'error', errorMessage: err.message });
         }
 
         res.status(200).json(lecturers);
+    });
+});
+
+/**
+ * @swagger
+ * /lecturers/add:
+ *   post:
+ *      summary: Add a lecturer
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Lecturer'
+ *      responses:
+ *         "200":
+ *            description: The ID of the new Lecturer
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: number
+ *                  description: Database ID
+ */
+lecturerRouter.post('/add', (req: Request, res: Response) => {
+    const lecturer = <Lecturer>req.body;
+    lecturerModel.addLecturer(lecturer, (err: Error, lecturerId: number) => {
+        if (err) {
+            return res.status(500).json({ status: 'error', errorMessage: err.message });
+        }
+
+        res.status(200).json({ status: 'success', lecturerId });
     });
 });
 
