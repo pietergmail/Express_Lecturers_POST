4d7408298d3840437ec702e4c5f4e806cea4b5ad
johanpck <johanpieck@gmail.com>
2022-02-22 13:39:01 +0100

    Add swagger

--- app.ts
+++ app.ts
@@ -2,10 +2,25 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { lecturerRouter } from './routes/lecturerRouter';
 
 const app = express();
 dotenv.config();
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API for Lecturers app',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,6 +29,14 @@ app.use('/lecturers', lecturerRouter);
 app.get('/status', (req, res) => {
     res.json({ message: 'Backend is running...' });
 });
+
+app.get('/', (req, res) => {
+    return res.status(200).send({
+        success: 'true',
+    });
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server is running on port ${process.env.APP_PORT}.`);
