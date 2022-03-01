138b382b5b332f3d53be6ec86d0776bbb5fded5a
johanpck <johanpieck@gmail.com>
2022-03-01 12:12:22 +0100

    Rename callback to onResult

--- model/lecturer.ts
+++ model/lecturer.ts
@@ -3,7 +3,7 @@ import mapToLecturers from './lecturer-mapper';
 import { Lecturer } from '../types';
 import { query } from '../database';
 
-const getLecturers = (callback: Function) => {
+const getLecturers = (onResult: Function) => {
     const queryStatement = `SELECT l.id AS lecturer_id, l.name AS lecturer_name, c.id AS course_id, c.name AS course_name, c.description AS course_description, c.phase AS course_phase
   FROM lecturer AS l, course AS c, lecturer_course AS lc
   WHERE l.id = lc.lecturer_id
@@ -11,11 +11,11 @@ const getLecturers = (callback: Function) => {
 
     (async () => {
         const rows = await query(queryStatement);
-        callback(null, mapToLecturers(rows));
-    })().catch((err) => callback(err));
+        onResult(null, mapToLecturers(rows));
+    })().catch((err) => onResult(err));
 };
 
-const addLecturer = (lecturer: Lecturer, callback: Function) => {
+const addLecturer = (lecturer: Lecturer, onResult: Function) => {
     const lecturerInsert = 'INSERT INTO lecturer (name) VALUES (?)';
     const lecturerCourseInsert =
         'INSERT INTO lecturer_course (lecturer_id, course_id) VALUES (?, ?)';
@@ -28,8 +28,8 @@ const addLecturer = (lecturer: Lecturer, callback: Function) => {
             async (course) => await query(lecturerCourseInsert, [addedLecturerId, course.id])
         );
 
-        callback(null, addedLecturerId);
-    })().catch((err) => callback(err));
+        onResult(null, addedLecturerId);
+    })().catch((err) => onResult(err));
 };
 
 export { getLecturers, addLecturer };
